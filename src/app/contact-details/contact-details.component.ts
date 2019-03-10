df755d0a824bd8f93f1a713e956263a4c2a93350
Pedro <pholik1@gmail.com>
2019-03-10 18:35:27 +0100

    nav left styling

--- src/app/contact-details/contact-details.component.ts
+++ src/app/contact-details/contact-details.component.ts
@@ -5,7 +5,7 @@ import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
-  styleUrls: ['./contact-details.component.css']
+  styleUrls: ['./contact-details.component.scss']
 })
 export class ContactDetailsComponent implements OnInit {
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { GlobalModule } from '../global';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 
 export class HomeComponent implements OnInit {
